72cf1d30c5582fa9341e7af414965ec26f94d1a9
Nami <namikaehler@outlook.com>
2022-01-14 23:29:54 +0100

    uwu

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Redirect.sh
+
 Redirect.sh is a shell script wich I use to redirect incomming traffic to my home Server, wich has a Dynamic IP and since we cant rout iptables with a domain I wrote that script.
 It Looks up the IP behind given Domain and uses the ports.ini file to create IP rules with the given port and protocoll (like TCP and UDP). You can choose different incomming and outgoing ports.
 
-## Example of the Port.ini
+## Example of the Ports.ini
+
 | Protocol | Incomming Port | Outgoing Port | Note declared with a ```-``` |
-| -------- | -------------- | ------------- | ---------------------------- |
-| TCP | 1234 | 4321 | -Website |
+|----------|----------------|---------------|------------------------------|
+| TCP      | 1234           | 4321          | -Website                     |
--- redirect.sh
+++ redirect.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# To automate this process you need to create a cronjob
 # CRONJOB (sudo crontab -e):
 # * * * * * /path/to/this/script
 
@@ -9,11 +10,11 @@ DYN_ADRESS=''
 #
 #
 # /////////////////////////////////////////////////////////
+
 SCRIPT_PATH="$(dirname $(readlink -f $0))"
 #text file
 PORTFILE="$SCRIPT_PATH/ports.ini"
 IPFILE='currentip.store'
-
 if test $(cat /proc/sys/net/ipv4/ip_forward) -eq 0; then
 	printf "\n\033[0;31mEnable IPv4 Forwarding to use Iptable rules\n"
 	printf "set 'net.ipv4.ip_forward = 1' in /etc/sysctl.conf\n"
@@ -21,24 +22,22 @@ if test $(cat /proc/sys/net/ipv4/ip_forward) -eq 0; then
 	printf "\033[0m\n\n"
 	exit 0
 fi
-if  ! test -f "$IPFILE"; then
+if ! test -f "$IPFILE"; then
 	touch "$IPFILE"
 fi
-if  ! test -f "$PORTFILE"; then
-		echo '#Only one config per line, like <Protocoll> <Ingoing Port> <Outgoing Port> <-Name>. Example "udp 1234 1234 -MinecraftServer". Commentout unused rules with "#"' > "$PORTFILE"
-		echo 'Commentout unused rules with "#"' >> "$PORTFILE"
-		echo '' >> "$PORTFILE"
-		echo '#udp 1234 1234 -Examle' >> "$PORTFILE"
+if ! test -f "$PORTFILE"; then
+	echo '#Only one config per line, like <Protocoll> <Ingoing Port> <Outgoing Port> <-Name>. Example "udp 1234 1234 -MinecraftServer". Commentout unused rules with "#"' >"$PORTFILE"
+	echo 'Commentout unused rules with "#"' >>"$PORTFILE"
+	echo '' >>"$PORTFILE"
+	echo '#udp 1234 1234 -Examle' >>"$PORTFILE"
 fi
-
 #Assign IP from resolving DynDNS to $DYN_IP
 DYN_IP="$(nslookup $DYN_ADRESS | grep 'Address: ' | cut -d ' ' -f2)"
 #
 CURRENT_IP="$(cat $SCRIPT_PATH/$IPFILE)"
 
 # Use: del_port <single iptable line>
-del_port()
-{
+del_port() {
 	echo "$1" | grep 'tcp' | \
 	while read line; do
 		SRC_PORT=$(echo "$line" | cut -d ':' -f2 | sed 's/ to//g')
@@ -46,42 +45,37 @@ del_port()
 		sudo iptables -t nat -D PREROUTING -i ens3 -p tcp --dport "$SRC_PORT" -j DNAT --to "$CURRENT_IP":"$DST_PORT"
 	done
 	echo "$1" | grep 'udp' | \
-        while read line; do
+	while read line; do
 		SRC_PORT=$(echo "$line" | cut -d ':' -f2 | sed 's/ to//g')
-        DST_PORT=$(echo "$line" | cut -d ':' -f4)
+		DST_PORT=$(echo "$line" | cut -d ':' -f4)
 		sudo iptables -t nat -D PREROUTING -i ens3 -p udp --dport "$SRC_PORT" -j DNAT --to "$CURRENT_IP":"$DST_PORT"
 	done
 }
 #Use: conf_port <line from PORTFILE>
-conf_port()
-{
+conf_port() {
 	sudo iptables -t nat -A PREROUTING -i ens3 -p "$1" --dport "$2" -j DNAT --to "$DYN_IP":"$3"
 }
-
 #echo "Current IP: $CURRENT_IP"
 #echo "Dynamic IP: $DYN_IP"
 #Check if the IP in the IPTABLES rules is still valid
-if [ "$CURRENT_IP" != "$DYN_IP" ] || [ "$(whoami)" != "root" ]
-then
+#Dirty check if the script is rub by a user or a cronjob
+if [ "$CURRENT_IP" != "$DYN_IP" ] || [ "$(whoami)" != "root" ]; then
 	#Filters iptable by old IP
 	sudo iptables -t nat -L | grep "to:$CURRENT_IP" | \
 	while IFS= read i; do
-			del_port "$i"
+		del_port "$i"
 	done
 	#Reads Ports  and Protocol from PORTFILE
-
 	cat $PORTFILE | grep -v '#' | cut -d '-' -f1 | \
 	while read line; do
-		if [ ! -z "$line" ]
-		then
+		if [ ! -z "$line" ]; then
 			conf_port $line
 		fi
 	done
 	sudo iptables -t nat -D POSTROUTING -o ens3 -j MASQUERADE
 	sudo iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
-	echo "$DYN_IP" > "$SCRIPT_PATH"/"$IPFILE"
-	
-	else
-		#echo "IPs are equal"
-		exit 0
+	echo "$DYN_IP" >"$SCRIPT_PATH"/"$IPFILE"
+else
+	#echo "IPs are equal"
+	exit 0
 fi
